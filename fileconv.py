b728ff2050208886dcfa1e76cb2ec2e604a79ed5
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-07-05 18:33:16 +0530

    Update fileconv.py

--- fileconv.py
+++ fileconv.py
@@ -232,7 +232,7 @@ def videoinfo(file):
     info = info.replace('\\n', '<br>')
     info = info.replace('\', \'','')
     file = file.split("downloads")[-1]
-    if file[0] == '/'
+    if file[0] == '/':
        file = file[1:]
     response = telegraph.create_page(f'{file}',html_content=f'<p>{info}</p>')
     return response['url']
