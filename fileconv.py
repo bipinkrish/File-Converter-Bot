c1ddfd70685627f3c571d602240f6d6b0735df6a
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-07-06 13:37:46 +0530

    Update fileconv.py

--- fileconv.py
+++ fileconv.py
@@ -181,7 +181,8 @@ def fontforgecommand(input,output):
 def libreofficecommand(input,new,output):
     #cmd = f'{libreoffice} --appimage-extract-and-run --headless --convert-to "{new}" "{input}" --outdir "{dirPath}"'
     if input.split(".")[-1] == 'pdf':
-        cmd = f'pdf2odt --pdf {input} {output}'
+        cmd = f'pdf2odt --pdf "{input}" "{output}"'
+	app.send_message(message.chat.id,cmd)
     else:
         cmd = f'libreoffice --headless --convert-to "{new}" "{input}" --outdir "{dirPath}"'
     print("Command to be Executed is")
