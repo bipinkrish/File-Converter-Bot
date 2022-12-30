70070e4e0cfe7168967f9431755512664eff6cd2
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-12-30 21:06:43 +0530

    Update chatGPT.py

--- chatGPT.py
+++ chatGPT.py
@@ -1,14 +1,22 @@
 from os import environ
+from threading import Semaphore
 from pyChatGPT import ChatGPT
 import chromedriver_autoinstaller
 chromedriver_autoinstaller.install()
 
 Session_Token = environ.get("Session_Token", None)
-if Session_Token != None: api = ChatGPT(Session_Token, verbose=True) 
+semaphore = Semaphore(1)
 
 def chatGPTget(question):
     if Session_Token == None: return "__chatGPT session is not set__"
 
-    resp = api.send_message(question)
-    return resp["message"]
-
+    semaphore.acquire()
+    try:
+        api = ChatGPT(Session_Token, verbose=True) 
+        resp = api.send_message(question)
+        api.clear_conversations()
+        return resp["message"]
+    except Exception as e:
+        return e
+    finally:
+        semaphore.release()
