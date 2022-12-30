5c8cb7f137af9fbd953b65dff0018b1f806b62c3
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-12-30 16:13:15 +0530

    Update chatGPT.py

--- chatGPT.py
+++ chatGPT.py
@@ -4,7 +4,7 @@ import chromedriver_autoinstaller
 chromedriver_autoinstaller.install()
 
 Session_Token = environ.get("Session_Token", None)
-if Session_Token != None: api = ChatGPT(Session_Token) 
+if Session_Token != None: api = ChatGPT(Session_Token, verbose=True) 
 
 def chatGPTget(question):
     if Session_Token == None: return "__chatGPT session is not set__"
